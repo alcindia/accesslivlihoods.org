2fb70622f020d076612bd3bf7568d22209ebf22c
rudrendra chaturvedi <rudrendra.chaturvedi@accesslivelihoods.org>
2024-02-15 15:04:19 +0530

    404 page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./Components/NavBar/NavBarOptions";
 import SocialMedia from "./pages/SocialMedia";
@@ -13,11 +13,12 @@ import About from "./pages/AboutUs/AboutUs";
 import Resources from "./pages/Resources/Resources";
 import DonateForm from "./pages/Donate";
 import Footer from "./Components/Footer/FooterPages/AclFooter";
+import NotFound from "./pages/NotFound";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
-  React.useEffect(() => {
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
@@ -28,27 +29,24 @@ const App = () => {
   useDocTitle("Access Livelihoods");
 
   return (
-    <>
-      <Router>
-        <ScrollToTop />
-        <Navbar />
-        <SocialMedia />
-
-        <Routes>
-          <Route path="/" element={<AlfHome />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/Resources" element={<Resources />} />
-          <Route path="/Innovations" element={<Innovations />} />
-          <Route path="/Models" element={<Models />} />
-          <Route path="/Clients" element={<Clients />} />
-          <Route path="/Careers" element={<Careers />} />
-          <Route path="/Donate" element={<DonateForm />} />
-        </Routes>
-
-        <Footer className="absolute mb-0 footer--pin gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 flex object-contain max-w-full mx-auto px-2 sm:px-6 shadow-md" />
-      </Router>
-    </>
+    <Router>
+      <ScrollToTop />
+      <Navbar />
+      <SocialMedia />
+      <Routes>
+        <Route path="/" element={<AlfHome />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Resources" element={<Resources />} />
+        <Route path="/Innovations" element={<Innovations />} />
+        <Route path="/Models" element={<Models />} />
+        <Route path="/Clients" element={<Clients />} />
+        <Route path="/Careers" element={<Careers />} />
+        <Route path="/Donate" element={<DonateForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer className="absolute mb-0 footer--pin gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 flex object-contain max-w-full mx-auto px-2 sm:px-6 shadow-md" />
+    </Router>
   );
 };
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='m-12 text-center font-black-bold '>
+      <h1>404. That’s an error.</h1> <br />
+      <p>The page you are looking for does not exist. </p> <br />
+    </div>
+  );
+};
+
+export default NotFound;
